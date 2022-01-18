1d588e2b1736dca5959959241849b85c1c2c32b2
Abdullah AlJasser <amj.jasser@gmail.com>
2022-01-18 11:23:08 -0500

    adding contact route

---
+++ routes/contact.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const ErrorResponse = require("../utils/errorResponse");
+const viewResponse = require("../utils/viewResponse");
+const asyncHandler = require("../middleware/async");
+
+const router = express.Router({ mergeParams: true });
+
+// @desc      Get about page
+// @route     GET /contact
+// @access    Public
+router.get(
+    "/",
+    asyncHandler(async (req, res, next) => {
+        viewResponse(req, res, next, "contact");
+    })
+);
+
+module.exports = router;
