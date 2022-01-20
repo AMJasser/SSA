0b2720128f3b80d807f96f7b5b4bda2aa10b8673
Abdullah AlJasser <amj.jasser@gmail.com>
2022-01-19 20:58:28 -0500

    protecting suggestion delete

--- routes/suggestions.js
+++ routes/suggestions.js
@@ -11,6 +11,6 @@ const { protect, protectAdmin } = require("../middleware/auth");
 
 router.get("/", getSuggestions);
 router.post("/", createSuggestion);
-router.delete("/:id", deleteSuggestion);
+router.delete("/:id", protect, protectAdmin, deleteSuggestion);
 
 module.exports = router;
