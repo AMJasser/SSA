79615ffc16aac1626919db6b474f3c8a93419c46
Abdullah AlJasser <amj.jasser@gmail.com>
2023-08-20 22:49:05 -0400

    Fix Auth Options

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,5 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import type { NextAuthOptions } from 'next-auth';
-
-export const authOptions: NextAuthOptions = {
-    secret: process.env.NEXTAUTH_SECRET,
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID || "",
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
-        })
-    ]
-};
+import { authOptions } from "@/utils/authOptions";
 
 const handler = NextAuth(authOptions);
 
