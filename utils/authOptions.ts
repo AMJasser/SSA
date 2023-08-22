154e1937154026385087ed7fc5b552dd7029fd9a
Abdullah AlJasser <amj.jasser@gmail.com>
2023-08-22 16:41:58 -0400

    Debug NextAuth

--- utils/authOptions.ts
+++ utils/authOptions.ts
@@ -6,6 +6,7 @@ import prisma from "./prisma";
 
 export const authOptions: NextAuthOptions = {
     secret: process.env.NEXTAUTH_SECRET,
+    debug: true,
     session: {
         strategy: "jwt",
         maxAge: 30 * 24 * 60 * 60 // 30 days
